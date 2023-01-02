8334ccdd562a37f8397846f06860bd825a0e3aee
Flowyuan <73770515+Flowyuan@users.noreply.github.com>
2023-01-02 15:46:30 +0800

    Initial commit

---
+++ src/main/java/com/example/javaee2/Javaee2Application.java
@@ -0,0 +1,13 @@
+package com.example.javaee2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaee2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javaee2Application.class, args);
+    }
+
+}
